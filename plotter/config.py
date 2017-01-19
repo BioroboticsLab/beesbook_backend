a634e251a4a31a8e172dc25fdca5fce3c542277a
Kadir Tugan <ktugan@users.noreply.github.com>
2017-01-19 15:08:34 +0100

    bugfix frame extraction start from 0

--- plotter/config.py
+++ plotter/config.py
@@ -8,7 +8,7 @@ ffmpeg_video = ' '.join([
     '{output_path}'  # output path
 ])
 
-ffmpeg_extract_all_frames = 'ffmpeg -v 24 -i {video_path} {output_path}/%04d.png'
+ffmpeg_extract_all_frames = 'ffmpeg -v 24 -i {video_path} -start_number 0 {output_path}/%04d.png'
 ffmpeg_extract_single_frame = 'ffmpeg -v 24 -i {video_path} -vf "select=gte(n\,{frame_index})" -vframes 1 {output_path}'
 
 ffmpeg_frames_to_video = 'ffmpeg -r 3 -i {input_path} -r 3 -vcodec libx264 {output_path}'
