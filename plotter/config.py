545cc0d18338d4fae96d5ff1895b2459ab3ea4bd
Kadir Tugan <ktugan@users.noreply.github.com>
2017-01-29 16:07:35 +0100

    refactored config file

--- plotter/config.py
+++ plotter/config.py
@@ -1,22 +1,44 @@
+scale = 0.5
+verbosity_level = 24
+
 ffmpeg_video = ' '.join([
-    'ffmpeg -v 24',  # only show warnings or higher
+    f'ffmpeg -v {verbosity_level}',
+    # '-vcodec hevc_cuvid',
     '-i {video_path}',  # input
     '-vf "select=gte(n\,{left_frame_idx}),setpts=PTS-STARTPTS" -vframes {number_of_frames}',  # subset selection
     '-crf 28',  # quality
     '-c:v h264',  # encoder
-    '-pix_fmt yuv420p',  # squash warning
+    '-pix_fmt yuv420p',  # for outdated players
     '{output_path}'  # output path
 ])
-scale = '0.5'
 
-ffmpeg_extract_all_frames = 'ffmpeg -v 24 -i {video_path} ' \
-                            '-start_number 0 ' \
-                            '-vf scale=iw*' + scale + ':ih*' + scale + ' ' \
-                            '-qscale:v 2 ' \
-                            '{output_path}/%04d.jpg'
+ffmpeg_extract_all_frames = ' '.join([
+    f'ffmpeg -v {verbosity_level}',
+    # '-vcodec hevc_cuvid',
+    '-r 3',
+    '-i {video_path}',
+    '-start_number 0',
+    f'-vf scale=iw*{scale}:ih*{scale}',
+    '-qscale:v 2',
+    '{output_path}/%04d.jpg'
+])
 
-ffmpeg_frames_to_video = 'ffmpeg -r 3 -i {input_path} -r 3 -vcodec libx264 {output_path}'
-ffmpeg_extract_single_frame = 'ffmpeg -v 24 -i {video_path} ' \
-                              '-vf "select=gte(n\,{frame_index}), scale=iw*' + scale + ':ih*' + scale + '" ' \
-                              '-vframes 1 {output_path}'
+ffmpeg_frames_to_video = ' '.join([
+    f'ffmpeg -v {verbosity_level}',
+    '-r 3',  # input framerate
+    '-i {input_path}',
+    '-r 3',  # video framerate (kinda irrelevant)
+    '-pix_fmt yuv420p',
+    '-vcodec libx264',
+    '{output_path}'
+])
 
+ffmpeg_extract_single_frame = ' '.join([
+    f'ffmpeg -v {verbosity_level}',
+    #  '-vcodec hevc_cuvid',
+    '-i {video_path}',
+    '-vf "select=gte(n\,{frame_index}),'  # first filter: select specific frame
+    f'scale=iw*{scale}:ih*{scale}"',  # second filter: scale output down or up
+    '-vframes 1',
+    '{output_path}'
+])
