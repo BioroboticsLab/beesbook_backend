5be56af8355401b41de13e08c8b746bf085d6b77
Kadir Tugan <ktugan@users.noreply.github.com>
2017-02-03 17:10:21 +0100

    admin models

--- plotter/admin.py
+++ plotter/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from plotter.models import Frame, Video, FrameContainer
+from plotter.models import FrameContainer
+
 # Register your models here.
+
+admin.site.register(Frame)
+admin.site.register(FrameContainer)
+admin.site.register(Video)
+
